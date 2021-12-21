8d99592f92548314ba1ef2bc88342445a9a7c751
Blessing Ajayi-Martins <blessmartins@gmail.com>
2021-12-21 12:50:59 -0500

    about component  no styling

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,11 +2,41 @@
 
 const About = () => {
   return (
-    <div>
-      4 divs. 
-    div 1 has the about me info - img 1 to the right
-    img 2 - div with measurements
-    </div>
+    <>
+      <div className="container-fluid row">
+      <div className="col">
+        <div>
+          <h1>
+            About Me
+          </h1>
+          <p>
+            Freelance Model
+          </p>
+          <p>
+          Welcome to my modeling portfolio. I started modeling in October 2019 and immediately fell in love with meeting talented creatives and helping to bring their ideas to life. My time is primarily spent in New York City, NY; Miami, FL; and Columbus, OH. I am also available to travel to other destinations in the United States and internationally. I have experience in editorial, runway, commercial, and pageantry.
+          </p>
+        </div>
+        <div>
+          IMG goes here
+        </div>
+
+
+      </div>
+      <div className="col">
+        <div>
+          IMG goes here
+        </div>
+        <div>
+          <h1>
+            MEASUREMENTS
+
+          </h1>
+
+        </div>
+      </div>
+      </div>
+    </>
+    
     
   )
 }
